5da862049a624fa11954bfff71c948b2414a6a9e
Dino Antonelli <dinoantonelli@hotmail.com>
2022-04-18 10:39:14 -0300

    Initial Commit. Setup and MySQl

---
+++ src/main/java/ar/dino/AplicacionCompletaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ar.dino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionCompletaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionCompletaSpringBootApplication.class, args);
+	}
+
+}
